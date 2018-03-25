530bf6efd54c6707fa372fd12e8b0abd04691f5a
Miles McCall <mccallm@oregonstate.edu>
2018-03-25 12:02:09 -0700

    added Makefile and cleaned novel_image.py

--- src/data_prep/novel_image.py
+++ src/data_prep/novel_image.py
@@ -8,29 +8,9 @@ description: Take a new set of images as input and prepares them as a tensorflow
 # External Imports
 import time
 import sys
-	#import tensorflow as tf
 # Local Imports
 from prepare_novel_data import PrepareData
 
-'''
-# Definitions and Constants
-CLASSES = ['bowl', 'calculator', 'cell_phone', 'notebook']
-NUM_OBJECTS = 5  # Number of different objects per object class
-IMAGE_HEIGHT = 112
-IMAGE_WIDTH = 112
-IMAGE_SIZE = IMAGE_WIDTH * IMAGE_HEIGHT
-COLOR_CHANNELS = 3
-WEIGHT_SIZE = 5
-BATCH_SIZE = 50
-KEEP_RATE = 0.8
-N_EPOCHS = 800
-FC_NEURON_SIZE = 1024  # Chosen randomly for now
-N_CLASSES = len(CLASSES)
-FC_NUM_FEATURES = IMAGE_WIDTH * IMAGE_HEIGHT * N_CLASSES
-DEFUALT_TRAIN_PATH = '../image_data/captured_cropped'
-VALIDATION_SIZE = .2
-LEARNING_RATE = .001
-'''
 
 # Definitions and Constants
 NUM_OBJECTS = 1  # Number of different objects per object class
