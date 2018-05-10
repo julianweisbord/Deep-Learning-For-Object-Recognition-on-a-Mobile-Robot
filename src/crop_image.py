b6e6eb4dab271132f3025f202e7942f2c9468e2b
Michael <rodrigmi@oregonstate.edu>
2018-05-10 13:45:08 -0700

    Added PEP8 formatting

--- src/crop_image.py
+++ src/crop_image.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 import glob
 
+
 def crop(image_path, coords, saved_location):
     """
     @param image_path: The path to the image to edit
@@ -17,52 +18,66 @@ def crop(image_path, coords, saved_location):
     cropped_image = image_obj.crop(coords)
     cropped_image.save(saved_location)
 
+
+'''
+    Main - Image cropping module
+    This function crops images for the 5 image classes of a given object. The
+    function takes an entire image class folder and loops through all 5
+    instances within it cropping each one and saving it in a new directory.
+    # Arguments
+        None
+    # Returns
+        Output: None
+'''
+
+
 def main():
 
     for instance_num in range(5):
 
-        window_scaling = 2.0
-        object_class = "screwdriver"
+        # Name of the object class to crop
+        object_class = "book"
         object_instance = str(instance_num+1)
 
-        home_dir = "Deep-Learning-For-Object-Recognition-on-a-Mobile-Robot/src/image_capture/lifelong_object_learning/"
+        # Directory of where the folder of images is located
+        home_dir = "/Volumes/SAN_MINI/"
 
         text_file_path = home_dir + "data_captured/" + object_class + "/" + object_class + "_" + object_instance + "/metadata/"
         image_file_path = home_dir + "data_captured/" + object_class + "/" + object_class + "_" + object_instance + "/images/"
         save_image_path = home_dir + "data_captured/cropped/" + object_class + "/" + object_class + "_" + object_instance + "/images/"
-        desired_img_size = 299
-
-        index = -1
+        desired_dims = 220  # This is the desired image dimensions (ex:220x220)
+        index = 0
 
         # grab all files in directory
         files = [f for f in listdir(text_file_path) if f.endswith(".txt")]
         for file in files:
 
-            index += 1
-            
-            #Break if we've reached the number of images in directory
+            # Break if we've reached the number of images in directory
             if index == 80:
-                break;
+                break
 
             file_base = splitext(file)[0]
             image_file_name = file_base + ".png"
-            print ("Index: ", index)
+            print("Index: ", index)  # Printing index of current current image
             lines = [line.rstrip('\n') for line in open(text_file_path + file)]
-                    
+
             image_file = image_file_path + image_file_name
             image_size = lines[1].split()
-            height = int(image_size[0])
+            height = int(image_size[0]) 
             width = int(image_size[1])
-            
-            image_filename = save_image_path + file_base +".png"
-            x1 = ((width/2) - (220/2))   
-            y1 = ((height/2) - (220/2))  #x1 and y1 give bottom left corner of pic
 
-            x2 = ((width/2) + (220/2))   
-            y2 = ((height/2) + (220/2))  #x2 and y2 give top right corner of pic
+            image_filename = save_image_path + file_base + ".png"
+            x1 = ((width/2) - (desired_dims/2))
+            y1 = ((height/2) - (desired_dims/2))  # x1 & y1 give bottom left corner of pic
 
-            #Call crop function on the image
-            crop(image_file,(x1,y1,x2,y2) ,image_filename) 
+            x2 = ((width/2) + (desired_dims/2))
+            y2 = ((height/2) + (desired_dims/2))  # x2 & y2 give top right corner of pic
+
+            # Call crop function on the image
+            crop(image_file, (x1, y1, x2, y2), image_filename)
+
+            index += 1
+
 
 if __name__ == "__main__":
     main()
